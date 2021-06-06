648714beb0a52bf3a243bbac1d3fbeff00dea915
Adam <adamwawrzyniak5@gmial.com>
2021-06-06 22:23:01 +0200

    Dodanie funkcji zapisu danych wraz z czasem pomiaru do pliku LOG_DHT11.txt

--- odczytDHT11.py
+++ odczytDHT11.py
@@ -1,13 +1,21 @@
 import Adafruit_DHT
 import time
+import datetime
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
-
+DHT_LOG = "LOG_DHT11.txt"
+LOG_FILE = open(DHT_LOG, "w")
+DATE = str(datetime.date.today())
+a = str("\n")
+LOG_FILE.write(DATE)
+LOG_FILE.write(a)
 while True:
-	humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-	if humidity is not None and temperature is not None:
-	    print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature,humidity)) #Printing data
-	else:
-	    print("Sensor failture. Check wiring.");
-	time.sleep(1); # Set time each every data read (sec)
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature,humidity)) #Printing data
+        LOG = [time.strftime("%H:%M  "), "Temp={0:0.1f}C Humidity={1:0.1f}\n".format(temperature,humidity)]
+        LOG_FILE.writelines(LOG) #Wiriting to file
+    else:
+        print("Sensor failture. Check wiring.");
+    time.sleep(1); # Set time each every data read (sec)
