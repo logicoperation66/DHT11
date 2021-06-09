a46983e2e4f64190ec0f7a6229c8092202d233aa
Adam <adamwawrzyniak5@gmial.com>
2021-06-10 00:01:18 +0200

    Refactoring

--- odczytDHT11.py
+++ odczytDHT11.py
@@ -1,21 +1,23 @@
+#Before you start it is necesary to install Adafruit library and rest below
+
 import Adafruit_DHT
 import time
 import datetime
 
+#Chose model of your sensor and to which pin it is connected
+
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
-DHT_LOG = "LOG_DHT11.txt"
-LOG_FILE = open(DHT_LOG, "w")
-DATE = str(datetime.date.today())
-a = str("\n")
-LOG_FILE.write(DATE)
-LOG_FILE.write(a)
+LOG_FILE = open("LOG.txt", "+a")
+LOG_Title = ["\tDate of measurment : ", str(datetime.date.today()), "\n"]
+LOG_FILE.writelines(LOG_Title)
+
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature,humidity)) #Printing data
-        LOG = [time.strftime("%H:%M  "), "Temp={0:0.1f}C Humidity={1:0.1f}\n".format(temperature,humidity)]
-        LOG_FILE.writelines(LOG) #Wiriting to file
+        print(time.strftime("%H:%M:%S"),f": Temp = {temperature}C // Humidity = {humidity}%") #Printing data
+        LOG = [time.strftime("%H:%M:%S"),f": Temp = {temperature}C // Humidity = {humidity}\n"]
+        LOG_FILE.writelines(LOG) #Writing data to LOG file
     else:
         print("Sensor failture. Check wiring.");
-    time.sleep(1); # Set time each every data read (sec)
+    time.sleep(3); # Set time each every data read (sec)
